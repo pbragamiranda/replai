56dbd46ea0867e401b3ed54eee456bb027552692
PedroMiranda <pbragamiranda@gmail.com>
2020-05-21 21:57:11 -0300

    adding routes

--- app/models/lai_request.rb
+++ app/models/lai_request.rb
@@ -1,3 +1,4 @@
 class LaiRequest < ApplicationRecord
   belongs_to :user
+  has_many :request_answer
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :public_data, only: [:new, :create, :index, :show, :destroy] do
+  	resources :comments, only: [:new, :create]
+  end
+  resources :lai_requests, only: [:new, :create, :index, :show, :destroy] do
+  	resources :request_answers, only: [:new, :create]
+  end
 end
