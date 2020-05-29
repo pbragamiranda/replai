454e6706db32fd24d0e74ca7e402a8b0252da6a4
leandro brito <leandroabrito89@gmail.com>
2020-05-28 22:15:31 -0300

    FUCKKKK

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@ import { loadDynamicBannerText } from '../components/banner';
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require("turbolinks").start()
 
 
 
@@ -14,6 +14,6 @@ require("channels")
 document.addEventListener('turbolinks:load', () => {
   // Call your JS functions here
   loadDynamicBannerText();
+  initUpdateNavbarOnScroll();
 });
 
-  initUpdateNavbarOnScroll();
