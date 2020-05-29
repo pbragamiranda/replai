12e5ae7f0148607631df8cc5cb3618fc9f2b3f33
leandro brito <leandroabrito89@gmail.com>
2020-05-28 21:48:05 -0300

    navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,18 @@
+import "bootstrap";
+
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+import { loadDynamicBannerText } from '../components/banner';
+
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import "bootstrap";
+
+
+
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  initUpdateNavbarOnScroll();
+  loadDynamicBannerText();
+});
