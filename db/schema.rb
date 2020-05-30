d7eaaad26f1262527880c191cd36c6fddee893ff
PedroMiranda <pbragamiranda@gmail.com>
2020-05-30 11:29:54 -0300

    index page public data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_23_175818) do
+ActiveRecord::Schema.define(version: 2020_05_29_000347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,7 +98,7 @@ ActiveRecord::Schema.define(version: 2020_05_23_175818) do
     t.index ["lai_request_id"], name: "index_request_answers_on_lai_request_id"
   end
 
-    create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -110,7 +110,6 @@ ActiveRecord::Schema.define(version: 2020_05_23_175818) do
     t.string "name"
     t.string "genre"
     t.date "birthdate"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
