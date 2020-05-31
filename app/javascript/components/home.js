cd9ea33f3a95e0408b36bda332397323ef294709
PedroMiranda <pbragamiranda@gmail.com>
2020-05-31 14:25:36 -0300

    landing page

--- app/javascript/components/home.js
+++ app/javascript/components/home.js
@@ -1,12 +1,12 @@
 import { annotate } from 'rough-notation';
 
 const e = document.querySelector('#text-highlight');
-const annotation = annotate(e, { type: 'highlight' });
+const annotation = annotate(e, { type: 'highlight', color: '#FFD54F' });
 annotation.show();
 
 const loadHighlightText = () => {
-  const e = document.querySelector('#text-highlight');
-	const annotation = annotate(e, { type: 'highlight', color: '#FFF176' });
+	const e = document.querySelector('#text-highlight');
+	const annotation = annotate(e, { type: 'highlight', color: '#FFD54F' });
 	annotation.show();
 }
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,9 @@
 import "bootstrap";
 
+import { loadHighlightText } from '../components/home';
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 import { loadDynamicBannerText } from '../components/banner';
-import { loadHighlightText } from '../components/home';
+
 
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
