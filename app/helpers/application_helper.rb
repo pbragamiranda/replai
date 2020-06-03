6ec0eca78230977d9463bfd5a77ab0f1faf42070
PedroMiranda <pbragamiranda@gmail.com>
2020-06-02 21:21:57 -0300

    seeds

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+require 'twitter'
+
 module ApplicationHelper
+	def send_tweet(twitter_account)
+		client = Twitter::REST::Client.new do |config|
+		  config.consumer_key        = CONSUMER_KEY
+		  config.consumer_secret     = CONSUMER_SECRET
+		  config.access_token        = ACCESS_TOKEN
+		  config.access_token_secret = ACCESS_TOKEN_SECRET
+		end
+		client.update("Olá, @#{twitter_account}. Tem uma pedido de acesso informação bla bla")
+	end
 end
