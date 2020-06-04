186472a9e4c6349b748b6b782cb609e174f6f0e1
Yuri Gravata <ygravata@gmail.com>
2020-06-04 18:58:57 -0300

    sytling dashboard

--- app/models/lai_request.rb
+++ app/models/lai_request.rb
@@ -2,7 +2,7 @@ class LaiRequest < ApplicationRecord
   belongs_to :user
   belongs_to :branch
   has_many :request_answers
-  validates :status, presence: true, inclusion: { in: %w(Editando Em\ andamento Atrasado Concluído) }
+  validates :status, presence: true, inclusion: { in: %w(Editando Enviado Atrasado Concluído) }
   CATEGORIES = %w[Transparência Financeira Saúde Educação Infraestrutura Segurança]
   validates :category, presence: true, inclusion: { in: CATEGORIES }
   ANONYMITY = [true, false]
