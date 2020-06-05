493128532ff110735977ce4cba3f813e801f115b
PedroMiranda <pbragamiranda@gmail.com>
2020-06-04 21:40:31 -0300

    fixing twitter bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,10 @@ require 'twitter'
 module ApplicationHelper
 	def send_tweet(twitter_account)
 		client = Twitter::REST::Client.new do |config|
-		  config.consumer_key        = CONSUMER_KEY
-		  config.consumer_secret     = CONSUMER_SECRET
-		  config.access_token        = ACCESS_TOKEN
-		  config.access_token_secret = ACCESS_TOKEN_SECRET
+		  config.consumer_key        = ENV["CONSUMER_KEY"]
+		  config.consumer_secret     = ENV["CONSUMER_SECRET"]
+		  config.access_token        = ENV["ACCESS_TOKEN"]
+		  config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
 		end
 		client.update("Olá, @#{twitter_account}. Tem uma pedido de acesso informação bla bla")
 	end
--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,8 +2,8 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["fucking"],
-    typeSpeed: 50,
+    strings: ["um direito de todo cidadão", "um dever dos órgãos públicos", "uma engrenagem pra participação popular"],
+    typeSpeed: 60,
     loop: true
   });
 }
