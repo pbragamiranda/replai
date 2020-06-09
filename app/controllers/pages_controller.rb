3fbafcef57112378909ed6e9178c3219b0d42c93
leandro brito <leandroabrito89@gmail.com>
2020-06-09 19:36:20 -0300

    add about-lai page and transfer text from home to that page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,7 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def sobre_a_lai
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/sobre-a-lai', to: "pages#sobre_a_lai"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :public_data, only: [:new, :create, :index, :show, :destroy] do
   	resources :comments, only: [:new, :create]
