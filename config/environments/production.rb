f18c7199a3c40a58e6ee05e61773ce382afd7e19
PedroMiranda <pbragamiranda@gmail.com>
2020-06-13 11:01:42 -0300

    mailer to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://replai.org" }
-  # config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
